91da8794cc63e6cd8a8c75777def4f2b0576e98b
Gabriel Ganne <gabriel.ganne@enea.com>
2017-12-19 10:28:23 +0100

    compare u16x8_zero_byte_mask vs vaddlvq_u16

--- neon_ethernet_frame_is_tagged.c
+++ neon_ethernet_frame_is_tagged.c
@@ -47,6 +47,53 @@ ethernet_frame_is_tagged_x2_neon(uint16_t const type0, uint16_t const type1)
     return vaddlvq_u16(rv) != 0;
 }
 
+static inline uint32_t
+u16x8_zero_byte_mask(uint16x8_t const input)
+{
+    uint8x16_t vall_one = vdupq_n_u8(0x0);
+    uint8x16_t res_values = { 0x01, 0x02, 0x04, 0x08,
+                              0x10, 0x20, 0x40, 0x80,
+                              0x01, 0x02, 0x04, 0x08,
+                              0x10, 0x20, 0x40, 0x80};
+
+    /* input --> [0x80, 0x40, 0x01, 0xf0, ... ] */
+    uint8x16_t test_result =
+        vreinterpretq_u8_u16(vceqq_u16(input, vreinterpretq_u16_u8(vall_one)));
+    uint8x16_t before_merge = vminq_u8(test_result, res_values);
+    /*
+      before_merge--> [0x80, 0x00, 0x00, 0x10, ... ]
+       u8x16 --> [a,b,c,d, e,f,g,h, i,j,k,l, m,n,o,p]
+       pair add until we have 2 uint64_t
+     */
+    uint16x8_t merge1 = vpaddlq_u8(before_merge);
+    /* u16x8-->  [a+b,c+d, e+f,g+h, i+j,k+l, m+n,o+p] */
+    uint32x4_t merge2 = vpaddlq_u16(merge1);
+    /* u32x4-->  [a+b+c+d, e+f+g+h, i+j+k+l, m+n+o+p] */
+    uint64x2_t merge3 = vpaddlq_u32(merge2);
+    /* u64x2-->  [a+b+c+d+e+f+g+h,  i+j+k+l+m+n+o+p]  */
+    return (uint32_t) (vgetq_lane_u64(merge3, 1) << 8) + vgetq_lane_u64(merge3, 0);
+}
+
+static inline int
+ethernet_frame_is_tagged_x2_neon_2(uint16_t const type0, uint16_t const type1)
+{
+    uint16x8_t const ethertype_mask = { ETHERNET_TYPE_VLAN,
+                                        ETHERNET_TYPE_DOT1AD,
+                                        ETHERNET_TYPE_VLAN_9100,
+                                        ETHERNET_TYPE_VLAN_9200,
+                                        /* duplicate for type1 */
+                                        ETHERNET_TYPE_VLAN,
+                                        ETHERNET_TYPE_DOT1AD,
+                                        ETHERNET_TYPE_VLAN_9100,
+                                        ETHERNET_TYPE_VLAN_9200};
+
+    uint16x8_t type_vect = vcombine_u16(vdup_n_u16(type0), vdup_n_u16(type1));
+
+    uint16x8_t rv = vceqq_u16(ethertype_mask, type_vect);
+
+    return (u16x8_zero_byte_mask(rv) != 0xffff);
+}
+
 static inline int
 ethernet_frame_is_tagged_neon_1(uint16_t const type)
 {
@@ -95,9 +142,9 @@ main()
     unsigned j;
     /* padded with 0 */
     uint16_t const table[N] = {ETHERNET_TYPE_VLAN, ETHERNET_TYPE_DOT1AD,
-                              ETHERNET_TYPE_VLAN_9100, ETHERNET_TYPE_VLAN_9200,
-                              ETHERNET_TYPE_VLAN, ETHERNET_TYPE_DOT1AD,
-                              ETHERNET_TYPE_VLAN_9100, ETHERNET_TYPE_VLAN_9200};
+                               ETHERNET_TYPE_VLAN_9100, ETHERNET_TYPE_VLAN_9200,
+                               ETHERNET_TYPE_VLAN, ETHERNET_TYPE_DOT1AD,
+                               ETHERNET_TYPE_VLAN_9100, ETHERNET_TYPE_VLAN_9200};
     int r[N];
 
     for (j = 0; j < N; j++)
